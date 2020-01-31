2c512455790215618ab6838bd5ec66701b85f42b
amad1101 <amadia46@gmail.com>
2020-01-30 22:07:11 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// function nested(row = 3, col = 3) {
+//   for (let index = 0; index < row; index++) {
+//     let toAdd = document.createElement("div");
+//     toAdd.innerHTML = "Nested";
+//     toAdd.setAttribute("class", "flex-row");
+//     toAdd.setAttribute("role", "cell");
+//     glb.appendChild(toAdd);
+//   }
+// }
